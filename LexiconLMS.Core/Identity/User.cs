ef44cf4e5f74d36564ec7e71a6846ab1439c6558
John Källström <johnkaellstrom@gmail.com>
2024-06-20 12:08:47 +0200

    User entity

--- LexiconLMS.Core/Identity/User.cs
+++ LexiconLMS.Core/Identity/User.cs
@@ -5,22 +5,12 @@ namespace LexiconLMS.Core.Identity
 {
     public class User : IdentityUser<int>
     {
-        [Required]
-        [MinLength(length: 3)]
-        public string FirstName { get; set; } = string.Empty;
-
-        [Required]
-        [MinLength(length: 3)]
-        public required string LastName { get; set; } = string.Empty;
-
-        // Computed property
-        public string FullName => $"{FirstName} {LastName}";
-        // TODO: Add RoleName?
-        public string SearchableString => $"{FirstName} {LastName} {Email}";
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
 
         // Navigation properties
         public int CourseId { get; set; }
-        public Course Course { get; set; } = new Course();
+        public Course Course { get; set; } = default!;
         public ICollection<Document> Documents { get; set; } = new List<Document>();
     }
 }
