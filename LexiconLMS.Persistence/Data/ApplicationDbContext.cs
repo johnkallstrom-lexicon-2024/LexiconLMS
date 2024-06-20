272da26b37f59b272f6cce9bb87be57d1053eefd
John Källström <johnkaellstrom@gmail.com>
2024-06-20 12:09:09 +0200

    Dbcontext

--- LexiconLMS.Persistence/Data/ApplicationDbContext.cs
+++ LexiconLMS.Persistence/Data/ApplicationDbContext.cs
@@ -1,10 +1,24 @@
+using LexiconLMS.Core.Entities;
 using LexiconLMS.Core.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LexiconLMS.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User>(options)
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Module> Modules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
     }
 }
--- LexiconLMS.Persistence/Data/ApplicationUser.cs
+++ LexiconLMS.Persistence/Data/ApplicationUser.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 namespace LexiconLMS.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<Guid>
     {
     }
 }
