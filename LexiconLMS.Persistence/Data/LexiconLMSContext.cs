4865df79e14e32227d487c3eab32355ab324cb3d
John Källström <johnkaellstrom@gmail.com>
2024-06-20 12:36:56 +0200

    Dbcontext

--- LexiconLMS.Persistence/Data/LexiconLMSContext.cs
+++ LexiconLMS.Persistence/Data/LexiconLMSContext.cs
@@ -1,3 +1,4 @@
+using LexiconLMS.Core.Entities;
 using LexiconLMS.Core.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -11,26 +12,15 @@ namespace LexiconLMS.Data
         {
         }
 
-        //public DbSet<Activity> Activities { get; set; }
-        //public DbSet<Course> Courses { get; set; }
-        //public DbSet<Document> Documents { get; set; }
-        //public DbSet<Module> Modules { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Module> Modules { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            //builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
-            RenameIdentityTables(builder);
-        }
-
-        private void RenameIdentityTables(ModelBuilder builder)
-        {
-            builder.Entity<User>().ToTable("Users");
-            builder.Entity<Role>().ToTable("Roles");
-            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
-            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
-            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
-            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
-            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
         }
     }
 }
