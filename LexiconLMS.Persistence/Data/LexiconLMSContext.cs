83c15b80a606ffa5d52273dc3c9c28528b4a8e52
John Källström <johnkaellstrom@gmail.com>
2024-06-20 12:23:05 +0200

    Rename dbcontext

--- LexiconLMS.Persistence/Data/LexiconLMSContext.cs
+++ LexiconLMS.Persistence/Data/LexiconLMSContext.cs
@@ -1,26 +1,24 @@
-using LexiconLMS.Core.Entities;
 using LexiconLMS.Core.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Emit;
 
 namespace LexiconLMS.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    public class LexiconLMSContext : IdentityDbContext<User, Role, int>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public LexiconLMSContext(DbContextOptions<LexiconLMSContext> options) : base(options)
         {
         }
 
-        public DbSet<Activity> Activities { get; set; }
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Document> Documents { get; set; }
-        public DbSet<Module> Modules { get; set; }
+        //public DbSet<Activity> Activities { get; set; }
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Document> Documents { get; set; }
+        //public DbSet<Module> Modules { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            //builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
             RenameIdentityTables(builder);
         }
 
