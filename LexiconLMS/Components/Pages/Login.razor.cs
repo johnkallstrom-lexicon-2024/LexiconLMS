89ca0ec8ba21820ba7ba5afaf139df834e4960b8
John Källström <johnkaellstrom@gmail.com>
2024-06-20 18:45:47 +0200

    Login component + model

---
+++ LexiconLMS/Components/Pages/Login.razor.cs
@@ -0,0 +1,9 @@
+﻿using LexiconLMS.Models;
+
+namespace LexiconLMS.Components.Pages
+{
+    public partial class Login
+    {
+        public LoginRequest Model { get; set; } = default!;
+    }
+}
---
+++ LexiconLMS/Models/LoginRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace LexiconLMS.Models
+{
+    public record LoginRequest(string Email, string Password);
+}
