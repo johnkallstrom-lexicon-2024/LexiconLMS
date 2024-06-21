b087799c15d95b4e412a3fcdf9216a986da9c829
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-06-21 14:59:09 +0200

    Initial commit for DesignTimeDbContextFactory

---
+++ LexiconLMS.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LexiconLMS.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<LexiconDbContext>
+    {
+        public LexiconDbContext CreateDbContext(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            path = Path.GetFullPath(Path.Combine(path, @"..\LexiconLMS"));
+            Console.WriteLine($"Creating design-time context at {path}.");
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LexiconDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new LexiconDbContext(builder.Options);
+        }
+    }
+}
