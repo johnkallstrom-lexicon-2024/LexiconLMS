dcb731106e0dea32d4b68e29eafbe835b025c283
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-06-22 11:36:39 +0200

    Updated Service to match Interface

--- LexiconLMS.Core/Services/CourseService.cs
+++ LexiconLMS.Core/Services/CourseService.cs
@@ -1,59 +1,106 @@
 ﻿using LexiconLMS.Core.Entities;
+using LexiconLMS.Core.Exceptions;
+using LexiconLMS.Core.Identity;
 using LexiconLMS.Core.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace LexiconLMS.Core.Services
 {
     public class CourseService : ICourseService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Course> _courseRepository;
 
+        // Generate code to user eager loading
         public CourseService(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _courseRepository = _unitOfWork.GetRepository<Course>() ?? throw new RepositoryNotFoundException("Course");
         }
 
-        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        public async Task<IEnumerable<Course>> GetCoursesAsync()
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            return await courseRepository.GetAllAsync();
+            // Example of selective eager loading based on use case
+            return await _courseRepository.Entities
+                .Include(c => c.Modules) // Assuming Modules are always needed
+                .ToListAsync();
         }
 
         public async Task<Course?> GetCourseAsync(int id)
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            return await courseRepository.GetAsync(id);
+            // Eager load only necessary entities
+            return await _courseRepository.Entities
+                .Include(c => c.Documents)
+                .Include(c => c.Modules)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<User>> GetStudentsAsync(Course course)
+        {
+            if (course.Users == null || !course.Users.Any())
+            {
+                var loadedCourse = await _courseRepository.Entities
+                    .Where(c => c.Id == course.Id)
+                    .Include(c => c.Users)
+                    .FirstOrDefaultAsync();
+
+                return loadedCourse?.Users.ToList() ?? Enumerable.Empty<User>();
+            }
+
+            return course.Users;
+        }
+
+        public async Task<IEnumerable<User>> GetStudentsAsync(int courseId)
+        {
+            return await GetStudentsAsync(await _courseRepository.Entities.Where(c => c.Id == courseId)?.FirstOrDefaultAsync()!
+                ?? throw new InvalidOperationException($"Course with ID {courseId} not found"));
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsAsync(int courseId)
+        {
+            var courseWithDocuments = await _courseRepository.Entities
+                .Where(c => c.Id == courseId)
+                .Include(c => c.Documents)
+                .FirstOrDefaultAsync();
+
+            return courseWithDocuments?.Documents.ToList() ?? Enumerable.Empty<Document>();
+        }
+
+        public async Task<IEnumerable<Module>> GetModulesAsync(int courseId)
+        {
+            var course = await _courseRepository.GetAsync(courseId);
+            return course?.Modules.ToList() ?? Enumerable.Empty<Module>();
+        }
+
+        public async Task<IEnumerable<Module>> GetModulesAsync(Course course)
+        {
+            return await Task.FromResult(course.Modules.ToList());
         }
 
         public async Task<IEnumerable<Course>> FindCoursesAsync(string searchString)
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            return await courseRepository.FindAsync(c => c.SearchableString.Contains(searchString));
+            return await _courseRepository.FindAsync(c => c.SearchableString.Contains(searchString));
         }
 
         public async Task AddCourseAsync(Course course)
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            await courseRepository.AddAsync(course);
+            course.Created = DateTime.UtcNow;
+            course.LastModified = DateTime.UtcNow;
+            await _courseRepository.AddAsync(course);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task UpdateCourseAsync(Course course)
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            await courseRepository.UpdateAsync(course);
+            course.LastModified = DateTime.UtcNow;
+            await _courseRepository.UpdateAsync(course);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task DeleteCourseAsync(Course course)
         {
-            var courseRepository = _unitOfWork.GetRepository<Course>();
-            await courseRepository.DeleteAsync(course);
-            await _unitOfWork.SaveChangesAsync();
+            await _courseRepository.DeleteAsync(course);
+            await _courseRepository.SaveChangesAsync();
         }
     }
 }
--- LexiconLMS.Core/Services/DocumentService.cs
+++ LexiconLMS.Core/Services/DocumentService.cs
@@ -1,56 +1,115 @@
 ﻿using LexiconLMS.Core.Entities;
+using LexiconLMS.Core.Exceptions;
 using LexiconLMS.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LexiconLMS.Core.Services
 {
     public class DocumentService : IDocumentService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Document> _documentRepository;
 
         public DocumentService(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _documentRepository = _unitOfWork.GetRepository<Document>() ?? throw new RepositoryNotFoundException("Document");
         }
 
         public async Task<IEnumerable<Document>> GetAllDocumentsAsync()
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            return await documentRepository.GetAllAsync();
+            return await _documentRepository.Entities
+                .Include(d => d.User)
+                .Include(d => d.Course)
+                .Include(d => d.Module)
+                .Include(d => d.Activity)
+                .AsNoTracking()
+                .ToListAsync();
         }
 
         public async Task<Document?> GetDocumentAsync(int id)
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            return await documentRepository.GetAsync(id);
+            return await _documentRepository.Entities
+                .Include(d => d.User)
+                .Include(d => d.Course)
+                .Include(d => d.Module)
+                .Include(d => d.Activity)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByCriteriaAsync(Expression<Func<Document, bool>> criteria)
+        {
+            return await _documentRepository.Entities
+                .Where(criteria)
+                .Include(d => d.User)
+                .Include(d => d.Course)
+                .Include(d => d.Module)
+                .Include(d => d.Activity)
+                .AsNoTracking()
+                .ToListAsync();
         }
 
         public async Task<IEnumerable<Document>> FindDocumentsAsync(string searchString)
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            return await documentRepository.FindAsync(d => d.SearchableString.Contains(searchString));
+            return await _documentRepository.Entities
+                .Where(d => EF.Functions.Like(d.SearchableString, $"%{searchString}%"))
+                .Include(d => d.User)
+                .Include(d => d.Course)
+                .Include(d => d.Module)
+                .Include(d => d.Activity)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByUserAsync(int userId)
+        {
+            return await _documentRepository.FindAsync(d => d.UserId == userId);
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByCourseAsync(int courseId)
+        {
+            return await _documentRepository.FindAsync(d => d.CourseId == courseId);
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByModuleAsync(int moduleId)
+        {
+            return await _documentRepository.FindAsync(d => d.ModuleId == moduleId);
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByActivityAsync(int activityId)
+        {
+            return await _documentRepository.FindAsync(d => d.ActivityId == activityId);
+        }
+
+        public async Task<TEntity?> GetDependency<TEntity>(int id) where TEntity : BaseEntity
+        {
+            var repository = _unitOfWork.GetRepository<TEntity>();
+            return await repository?.GetAsync(id)!;
+        }
+
+        public async Task<IEnumerable<Document>> FindDocumentAsync(Expression<Func<Document, bool>> predicate)
+        {
+            return await _documentRepository.FindAsync(predicate);
         }
 
         public async Task AddDocumentAsync(Document document)
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            await documentRepository.AddAsync(document);
+            await _documentRepository.AddAsync(document);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task UpdateDocumentAsync(Document document)
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            await documentRepository.UpdateAsync(document);
+            await _documentRepository.UpdateAsync(document);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task DeleteDocumentAsync(Document document)
         {
-            var documentRepository = _unitOfWork.GetRepository<Document>();
-            await documentRepository.DeleteAsync(document);
+            await _documentRepository.DeleteAsync(document);
             await _unitOfWork.SaveChangesAsync();
         }
     }
-
-
 }
