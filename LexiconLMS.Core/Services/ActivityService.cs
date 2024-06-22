e8986b127d93b1a296dadaafbba4e5eec638b2dc
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-06-22 11:36:39 +0200

    Updated Service to match Interface

--- LexiconLMS.Core/Services/ActivityService.cs
+++ LexiconLMS.Core/Services/ActivityService.cs
@@ -1,55 +1,71 @@
 ﻿using LexiconLMS.Core.Entities;
+using LexiconLMS.Core.Exceptions;
 using LexiconLMS.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LexiconLMS.Core.Services
 {
     public class ActivityService : IActivityService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Activity> _activityRepository;
 
         public ActivityService(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _activityRepository = _unitOfWork.GetRepository<Activity>() ?? throw new RepositoryNotFoundException("Activity");
         }
 
-        public async Task<IEnumerable<Activity>> GetAllActivitiesAsync()
+        public async Task<IEnumerable<Activity>> GetActivitiesAsync()
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            return await activityRepository.GetAllAsync();
+            return await _activityRepository.Entities
+                .Include(a => a.Documents)
+                .ToListAsync();
         }
 
         public async Task<Activity?> GetActivityAsync(int id)
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            return await activityRepository.GetAsync(id);
+            return await _activityRepository.Entities
+                .Include(a => a.Documents)
+                .FirstOrDefaultAsync(a => a.Id == id);
         }
 
         public async Task<IEnumerable<Activity>> FindActivitiesAsync(string searchString)
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            return await activityRepository.FindAsync(a => a.SearchableString.Contains(searchString));
+            return await _activityRepository.Entities
+                .Include(a => a.Documents)
+                .Where(a => a.Name.Contains(searchString))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Activity>> FindActivitiesAsync(Expression<Func<Activity, bool>> predicate)
+        {
+            // Consider adding .Include(a => a.Documents) if related documents are needed
+            return await _activityRepository.FindAsync(predicate);
         }
 
         public async Task AddActivityAsync(Activity activity)
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            await activityRepository.AddAsync(activity);
+            // Consider adding validation here
+            activity.Created = DateTime.UtcNow;
+            activity.LastModified = DateTime.UtcNow;
+            await _activityRepository.AddAsync(activity);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task UpdateActivityAsync(Activity activity)
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            await activityRepository.UpdateAsync(activity);
+            // Consider adding validation here
+            activity.LastModified = DateTime.UtcNow;
+            await _activityRepository.UpdateAsync(activity);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task DeleteActivityAsync(Activity activity)
         {
-            var activityRepository = _unitOfWork.GetRepository<Activity>();
-            await activityRepository.DeleteAsync(activity);
+            await _activityRepository.DeleteAsync(activity);
             await _unitOfWork.SaveChangesAsync();
         }
     }
-
 }
--- LexiconLMS.Core/Services/ModuleService.cs
+++ LexiconLMS.Core/Services/ModuleService.cs
@@ -1,53 +1,86 @@
 ﻿using LexiconLMS.Core.Entities;
+using LexiconLMS.Core.Exceptions;
 using LexiconLMS.Core.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace LexiconLMS.Core.Services
 {
     public class ModuleService : IModuleService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Module> _moduleRepository;
 
         public ModuleService(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _moduleRepository = _unitOfWork.GetRepository<Module>() ?? throw new RepositoryNotFoundException("Module");
         }
 
-        public async Task<IEnumerable<Module>> GetAllModulesAsync()
+        public async Task<IEnumerable<Module>> GetModulesAsync()
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            return await moduleRepository.GetAllAsync();
+            return await _moduleRepository.Entities.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Module>> GetModulesAsync(int courseId)
+        {
+            return await _moduleRepository.Entities
+                .Where(m => m.CourseId == courseId)
+                .Include(m => m.Activities)
+                .Include(m => m.Documents)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Activity>> GetActivitiesAsync(int moduleId)
+        {
+            var module = await _moduleRepository.Entities
+                .Where(m => m.Id == moduleId)
+                .Include(m => m.Activities)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            return module?.Activities ?? Enumerable.Empty<Activity>();
+        }
+
+        public async Task<IEnumerable<Document>> GetDocumentsByModuleAsync(int moduleId)
+        {
+            var module = await _moduleRepository.Entities
+                .Where(m => m.Id == moduleId)
+                .Include(m => m.Documents)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            return module?.Documents ?? Enumerable.Empty<Document>();
         }
 
         public async Task<Module?> GetModuleAsync(int id)
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            return await moduleRepository.GetAsync(id);
+            return await _moduleRepository.Entities
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
         }
 
         public async Task<IEnumerable<Module>> FindModulesAsync(string searchString)
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            return await moduleRepository.FindAsync(m => m.SearchableString.Contains(searchString));
+            return await _moduleRepository.Entities
+                .Where(m => m.SearchableString.Contains(searchString))
+                .AsNoTracking()
+                .ToListAsync();
         }
 
         public async Task AddModuleAsync(Module module)
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            await moduleRepository.AddAsync(module);
+            await _moduleRepository.AddAsync(module);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task UpdateModuleAsync(Module module)
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            await moduleRepository.UpdateAsync(module);
+            await _moduleRepository.UpdateAsync(module);
             await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task DeleteModuleAsync(Module module)
         {
-            var moduleRepository = _unitOfWork.GetRepository<Module>();
-            await moduleRepository.DeleteAsync(module);
+            await _moduleRepository.DeleteAsync(module);
             await _unitOfWork.SaveChangesAsync();
         }
     }
