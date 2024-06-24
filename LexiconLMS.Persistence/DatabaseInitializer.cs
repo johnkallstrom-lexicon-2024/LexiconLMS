7a58746e9666e2d3bbcc1bdd3eddadd0b53e4eef
John Källström <johnkaellstrom@gmail.com>
2024-06-24 11:32:47 +0200

    Database initializer

---
+++ LexiconLMS.Persistence/DatabaseInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using LexiconLMS.Persistence.Fakers;
+using Microsoft.AspNetCore.Identity;
+
+namespace LexiconLMS.Persistence
+{
+    public class DatabaseInitializer
+    {
+        private static UserFaker _userFaker = new();
+        private static Faker _faker = new();
+
+        public static async Task SeedIdentityDataAsync(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            await CreateRoles(roleManager);
+        }
+
+        public static async Task CreateRoles(RoleManager<Role> roleManager)
+        {
+            string[] roles = ["Teacher", "Student"];
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new Role
+                    {
+                        Name = role,
+                        NormalizedName = role,
+                    });
+                }
+            }
+        }
+
+        public static async Task CreateUser(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            var users = _userFaker.Generate(100);
+            if (users != null && users.Count() > 0)
+            {
+                foreach (var user in users)
+                {
+                    user.PasswordHash = userManager.PasswordHasher.HashPassword(user, _faker.Internet.Password());
+
+                    var identityResult = await userManager.CreateAsync(user);
+                    if (identityResult.Succeeded)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LexiconLMS.Persistence/Fakers/UserFaker.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+
+namespace LexiconLMS.Persistence.Fakers
+{
+    public class UserFaker : Faker<User>
+    {
+        public UserFaker()
+        {
+            RuleFor(u => u.UserName, f => f.Person.UserName);
+            RuleFor(u => u.FirstName, f => f.Person.FirstName);
+            RuleFor(u => u.LastName, f => f.Person.LastName);
+            RuleFor(u => u.Email, f => f.Person.Email);
+        }
+    }
+}
