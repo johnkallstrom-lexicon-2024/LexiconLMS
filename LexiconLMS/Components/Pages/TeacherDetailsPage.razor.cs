cc12c358bb95074f70ef5cec607391e0dcba3bfc
John Källström <johnkaellstrom@gmail.com>
2024-06-26 17:52:15 +0200

    Teacher components

---
+++ LexiconLMS/Components/Pages/TeacherDetailsPage.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LexiconLMS.Components.Pages
+{
+    public partial class TeacherDetailsPage
+    {
+        [Parameter]
+        public int Id { get; set; }
+    }
+}
