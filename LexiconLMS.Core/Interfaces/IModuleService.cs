939e3c09739ea229f8ee49a2f02d5f6927ac6be5
John Källström <johnkaellstrom@gmail.com>
2024-06-27 14:34:54 +0200

    Document and module serbvices

--- LexiconLMS.Core/Interfaces/IModuleService.cs
+++ LexiconLMS.Core/Interfaces/IModuleService.cs
@@ -2,5 +2,10 @@
 {
     public interface IModuleService
     {
+        Task<IEnumerable<Module>> GetModulesAsync();
+        Task<Module?> GetModuleByIdAsync(int id);
+        Task<Module> CreateModuleAsync(Module module);
+        Task UpdateModuleAsync(Module module);
+        Task DeleteModuleAsync(Module module);
     }
 }
