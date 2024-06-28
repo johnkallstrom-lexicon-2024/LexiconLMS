d7d961df165c32de288312ff8a549bf160f77987
John Källström <johnkaellstrom@gmail.com>
2024-06-28 16:13:18 +0200

    Override SaveChangesAsync to automatically set Created or LastModified properties when inserting/updating entities to db

--- LexiconLMS.Persistence/Data/LexiconDbContext.cs
+++ LexiconLMS.Persistence/Data/LexiconDbContext.cs
@@ -19,5 +19,25 @@ namespace LexiconLMS.Persistence.Data
             builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
             builder.RenameIdentityTables();
         }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseEntity && e.State == EntityState.Added || e.State == EntityState.Modified)
+                .ToList();
+
+            foreach (var entry in entries)
+            {
+                var entity = entry.Entity as BaseEntity;
+                if (entity != null)
+                {
+                    if (entry.State == EntityState.Added) entity.Created = DateTime.Now;
+                    if (entry.State == EntityState.Modified) entity.LastModified = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
