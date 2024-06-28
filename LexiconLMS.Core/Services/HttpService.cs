a3a9fe520f857d4e7873ec1624ba886cc3326faf
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-06-28 11:27:13 +0200

    Added folder in Blazor project for all HttpServices, Added HttpService.cs to use generic http interface

---
+++ LexiconLMS.Core/Services/HttpService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Core.Services
+{
+    public class HttpService<TEntity> : IHttpService<TEntity> where TEntity : BaseEntity
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<IEnumerable<TEntity>>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<TEntity?> GetAsync(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<TEntity>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task PostAsync(string url, object data)
+        {
+            var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task PutAsync(string url, object data)
+        {
+            var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync(url, content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteAsync(string url)
+        {
+            var response = await _httpClient.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
