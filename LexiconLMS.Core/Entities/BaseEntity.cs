4049ce3b58ab384b1b81fe2298d8a3f86ab8876a
John Källström <johnkaellstrom@gmail.com>
2024-06-28 16:27:34 +0200

    LastModified as nullable

--- LexiconLMS.Core/Entities/BaseEntity.cs
+++ LexiconLMS.Core/Entities/BaseEntity.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public DateTime Created { get; set; }
-        public DateTime LastModified { get; set; }
+        public DateTime? LastModified { get; set; }
     }
 }
