c1dfe36f20e428e1fb44a8f4cce5f0b1e5f4ccf7
John Källström <johnkaellstrom@gmail.com>
2024-06-28 12:55:35 +0200

    Simplified http service

--- LexiconLMS.Core/Interfaces/IHttpService.cs
+++ LexiconLMS.Core/Interfaces/IHttpService.cs
@@ -2,7 +2,6 @@
 {
     public interface IHttpService
     {
-        Task<IEnumerable<TModel>> GetListAsync<TModel>(string url);
         Task<TModel?> GetAsync<TModel>(string url);
         Task PostAsync<TModel>(string url, TModel data);
         Task PutAsync<TModel>(string url, TModel data);
--- LexiconLMS.Core/Services/HttpService.cs
+++ LexiconLMS.Core/Services/HttpService.cs
@@ -11,34 +11,17 @@ namespace LexiconLMS.Core.Services
             _httpClient = httpClient;
         }
 
-        public async Task<IEnumerable<TModel>> GetListAsync<TModel>(string url)
-        {
-            var httpResponse = await _httpClient.GetAsync(url);
-            if (httpResponse.IsSuccessStatusCode)
-            {
-                var data = await httpResponse.Content.ReadFromJsonAsync<IEnumerable<TModel>>();
-                if (data != null)
-                {
-                    return data;
-                }
-            }
-
-            return Enumerable.Empty<TModel>();
-        }
-
         public async Task<TModel?> GetAsync<TModel>(string url)
         {
+            TModel? data = default;
+
             var httpResponse = await _httpClient.GetAsync(url);
             if (httpResponse.IsSuccessStatusCode)
             {
-                var data = await httpResponse.Content.ReadFromJsonAsync<TModel>();
-                if (data != null)
-                {
-                    return data;
-                }
+                data = await httpResponse.Content.ReadFromJsonAsync<TModel>();
             }
 
-            return default;
+            return data;
         }
 
         public async Task PostAsync<TModel>(string url, TModel data) => await _httpClient.PostAsJsonAsync(url, data);
