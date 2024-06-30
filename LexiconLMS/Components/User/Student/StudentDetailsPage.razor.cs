4e697d76db743c3901e07f3fb029ea15aff56ed9
John Källström <johnkaellstrom@gmail.com>
2024-06-30 16:59:06 +0200

    Student Overview and Details Page

--- LexiconLMS/Components/User/Student/StudentDetailsPage.razor.cs
+++ LexiconLMS/Components/User/Student/StudentDetailsPage.razor.cs
@@ -1,10 +1,37 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace LexiconLMS.Components.Student
+namespace LexiconLMS.Components.User.Student
 {
     public partial class StudentDetailsPage
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
         [Parameter]
         public int Id { get; set; }
+
+        public bool Loading { get; set; }
+        public UserModel Model { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetStudent();
+        }
+
+        private void Return() => NavigationManager.NavigateTo("/students");
+
+        private async Task GetStudent()
+        {
+            Loading = true;
+            var student = await HttpService.GetAsync<UserModel>($"{Endpoints.Users}/{Id}");
+            if (student != null)
+            {
+                Model = student;
+            }
+            Loading = false;
+        }
     }
 }
