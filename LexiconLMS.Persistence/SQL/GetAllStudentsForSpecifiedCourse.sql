a4d60b6b4529f293da5b2d99190d2446cfc8566c
John Källström <johnkaellstrom@gmail.com>
2024-06-30 15:18:29 +0200

    SQL query

---
+++ LexiconLMS.Persistence/SQL/GetAllStudentsForSpecifiedCourse.sql
@@ -0,0 +1,15 @@
+﻿SELECT 
+U.FirstName,
+U.LastName,
+U.Email,
+U.UserName,
+R.Name AS 'Role',
+C.Id AS 'Course Id',
+C.Name AS 'Course',
+C.StartDate,
+C.EndDate
+FROM Courses AS C
+INNER JOIN Users AS U ON U.CourseId = C.Id
+INNER JOIN UserRoles AS UR ON U.Id = UR.UserId
+INNER JOIN Roles AS R ON R.Id = UR.RoleId
+WHERE C.Id = 9
