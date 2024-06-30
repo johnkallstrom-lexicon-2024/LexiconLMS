3a9d12cea337f7dee10cd4f88dd40240169dad60
John Källström <johnkaellstrom@gmail.com>
2024-06-30 20:29:55 +0200

    Validation

--- LexiconLMS.Core/Models/Authenticate/AuthenticateRequest.cs
+++ LexiconLMS.Core/Models/Authenticate/AuthenticateRequest.cs
@@ -2,10 +2,10 @@
 {
     public class AuthenticateRequest : BaseModel
     {
-        [Required]
+        [Required(ErrorMessage = "Please enter a valid email")]
         public string Email { get; set; } = default!;
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a correct password")]
         public string Password { get; set; } = default!;
     }
 }
