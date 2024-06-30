cd772903e9638defeb5160392fb18423ed446799
John Källström <johnkaellstrom@gmail.com>
2024-06-30 17:29:47 +0200

    Result class

---
+++ LexiconLMS.Core/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace LexiconLMS.Core.Results
+{
+    public class Result
+    {
+        public bool Success { get; }
+        public List<string> Errors { get; } = [];
+
+        public Result()
+        {
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public Result(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public static Result Ok() => new Result(success: true);
+        public static Result Fail(List<string> errors) => new Result(errors);
+    }
+}
