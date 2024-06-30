0217408a47318a4ccc7d51ee3b84e9699f2bdacf
John Källström <johnkaellstrom@gmail.com>
2024-06-30 19:01:27 +0200

    Add auth attribute and jwt middleware

---
+++ LexiconLMS.Api/Authorization/JwtMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace LexiconLMS.Api.Authorization
+{
+    public class JwtMiddleware
+    {
+        private readonly IJwtProvider _jwtProvider;
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next, IJwtProvider jwtProvider)
+        {
+            _next = next;
+            _jwtProvider = jwtProvider;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string? token = context.Request.Headers.Authorization.ToString().Split(" ").Last();
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                // Validate token
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class JwtMiddlewareExtensions
+    {
+        public static void UseJwtMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<JwtMiddleware>();
+        }
+    }
+}
