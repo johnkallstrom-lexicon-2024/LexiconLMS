b4654a2f58a48bf18db0e770fa90495d6e3f28da
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-06-30 15:33:49 +0200

    Made createCorse working 100%

---
+++ LexiconLMS/Components/Course/CreateCourse.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace LexiconLMS.Components.Course
+{
+    public partial class CreateCourse
+    {
+        [SupplyParameterFromForm]
+        public CourseCreateModel Course { get; set ; }
+
+        [Inject]
+        public IHttpService? HttpService { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Course ??= new();
+        }
+        public async Task OnSumbit()
+        {
+            await HttpService.PostAsync(Endpoints.Courses, Course);
+            NavigationManager.NavigateTo("/teachers");
+        }
+      
+    }
+}
