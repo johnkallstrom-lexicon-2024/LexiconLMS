fe5f9837d4a8b5d651953c6cc2e12a4d329f3b21
John Källström <johnkaellstrom@gmail.com>
2024-06-30 17:37:58 +0200

    Result class

--- LexiconLMS.Core/Results/Result.cs
+++ LexiconLMS.Core/Results/Result.cs
@@ -1,6 +1,6 @@
 ﻿namespace LexiconLMS.Core.Results
 {
-    public class Result
+    public class Result : IResult
     {
         public bool Success { get; }
         public List<string> Errors { get; } = [];
@@ -19,7 +19,32 @@
             Errors = errors;
         }
 
-        public static Result Ok() => new Result(success: true);
+        public static Result Ok() => new Result(true);
         public static Result Fail(List<string> errors) => new Result(errors);
+    }
+
+    public class Result<TValue> : IResult<TValue>
+    {
+        public bool Success { get; }
+        public List<string> Errors { get; } = [];
+        public TValue Value { get; } = default!;
+
+        public Result()
+        {
+        }
+
+        public Result(bool success, TValue value)
+        {
+            Success = success;
+            Value = value;
+        }
+
+        public Result(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public static Result<TValue> Ok(TValue value) => new Result<TValue>(true, value);
+        public static Result<TValue> Fail(List<string> errors) => new Result<TValue>(errors);
     }
 }
