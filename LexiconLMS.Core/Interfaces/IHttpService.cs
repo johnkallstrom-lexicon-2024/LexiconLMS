bfe67db953c7c8f2a02f4f1fb379f464d1621f7b
John Källström <johnkaellstrom@gmail.com>
2024-06-30 20:01:13 +0200

    Http Service

--- LexiconLMS.Core/Interfaces/IHttpService.cs
+++ LexiconLMS.Core/Interfaces/IHttpService.cs
@@ -4,7 +4,6 @@
     {
         Task<TData?> GetAsync<TData>(string url);
         Task PostAsync(string url, object data);
-        Task<TValue?> PostAsync<TValue>(string url, object data);
         Task PutAsync(string url, object data);
         Task DeleteAsync(string url);
     }
