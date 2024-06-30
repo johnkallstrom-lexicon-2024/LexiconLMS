db4a160e36068066daccec3609bf12fed68659ec
John Källström <johnkaellstrom@gmail.com>
2024-06-30 17:29:55 +0200

    Auth controller

---
+++ LexiconLMS.Api/Controllers/AuthenticateController.cs
@@ -0,0 +1,22 @@
+﻿using LexiconLMS.Core.Models.User;
+
+namespace LexiconLMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthenticateController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginUser(LoginModel model)
+        {
+            return Ok();
+        }
+    }
+}
